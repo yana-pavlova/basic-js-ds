615de82face879d9bf317357039cb959b0a4e180
Yana Pavlova <yn-pvlv@yandex.ru>
2025-01-03 15:08:41 +0300

    fix: use built-in Node class to creates Node instances

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,18 +1,11 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.left = null;
-    this.right = null;
-  }
-}
 
 class BinarySearchTree {
   constructor() {
